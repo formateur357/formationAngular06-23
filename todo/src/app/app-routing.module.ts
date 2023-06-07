0fc0c09e338d8b3eb316f9d823d21a18c21298dd
formateur357 <formateur357@gmail.com>
2023-06-07 10:36:22 +0200

    correction exo nav

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodolistComponent } from './components/todolist/todolist.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "login", component: LoginComponent},
+  {path: "todolist", component: TodolistComponent},
+  {path: "", component: TodolistComponent, pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- todo/src/app/services/auth.service.ts
+++ todo/src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -6,17 +7,19 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   public isAuth: boolean
 
-  constructor() {
+  constructor(private router: Router) {
     this.isAuth = true
   }
 
   public login(): void {
     setTimeout(() => {
       this.isAuth = true
+      this.router.navigate(['todolist'])
     }, 1000)
   }
 
   public logout(): void {
     this.isAuth = false
+    this.router.navigate(['login'])
   }
 }
